529ccdbafa39ad89f2dca3185b9363bae8b410ee
Patrick Beagan <pbeagan1@gmail.com>
2022-07-13 16:13:04 -0500

    getting proper scaling in place, committing before converting to inches (for printing)

--- main.py
+++ main.py
@@ -30,9 +30,12 @@ def create_qr_codes(items):
 def write_to_file(items):
     with open(in_build_dir("index.html"), "w") as f:
         f.write("<html>")
-        f.write("<body class=\"masonry\">")
+        f.write("<body>")
+        f.write("<div class=\"masonry\">")
         write_css(f)
         write_single_item(f, items)
+        f.write("</div>")
+        f.write("""<div style="width: 8.5in; background: yellow">hello world</div>""")
         f.write("</body>")
         f.write("</html>")
 
@@ -52,30 +55,30 @@ img.qr {
     display: inline-block;
     flex-wrap: wrap;
     background-color: white;
-    column-count: 3;
+    column-count: 4;
     column-gap: 0px;
     padding-bottom: 20px;
 }       
-.masonry > a {
+.masonry > div > a {
     width: 300px;
     display: flex;
     text-align: center;
     overflow: hidden;
     background-color: #ffffff;
-    margin: 10px;
+    margin: 8px;
     overflow-wrap: break-word;
     break-inside: avoid;
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.1), 0 4px 4px 0 rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     border: 1px solid #aaaaaa;
 }
-a > div {
+div > a {
     break-inside: avoid;
 }
 div > p {
     text-align: left;
 }
-p{
+p {
     inline-size: 280px;
     margin: 2px 10px 2px 10px;
 }
@@ -93,27 +96,28 @@ p{
     top: 50%;
     transform: translateY(-50%);
 }
-@media only screen and (min-width: 1240px) {
+@media only screen and (min-width: 1321px) {
   .masonry {
     column-count: 4;
   }
 }
-
-@media only screen and (min-width: 930px) and (max-width: 1239px) {
+@media only screen and (min-width: 981px) and (max-width: 1320px) {
   .masonry {
     column-count: 3;
   }
 }
-
-@media only screen and (min-width: 620px) and (max-width: 929px) {
+@media only screen and (min-width: 641px) and (max-width: 980px) {
   .masonry {
     column-count: 2;
   }
 }
-@media only screen and (min-width: 0px) and (max-width: 619px) {
+@media only screen and (min-width: 0px) and (max-width: 640px) {
   .masonry {
     column-count: 1;
   }
+}
+.card-wrapper {
+    display: inline-block;
 }
 </style>
 """)
@@ -122,17 +126,19 @@ p{
 def write_single_item(f, items):
     for index, each in enumerate(items):
         f.write(f"""
-<a href="{each[0]}">
-    <div>
-        <div class="img-row">
-            <div class="img-sizer">
-                <img class="centered-element" src="{each[1]}"/> 
+<div class="card-wrapper">
+    <a href="{each[0]}">
+        <div>
+            <div class="img-row">
+                <div class="img-sizer">
+                    <img class="centered-element" src="{each[1]}"/> 
+                </div>
+                <img class="qr" src="{get_qr_output_file_name(index)}"/>
             </div>
-            <img class="qr" src="{get_qr_output_file_name(index)}"/>
+            <p>{each[0]}</p>
         </div>
-        <p>{each[0]}</p>
-    </div>
-</a>
+    </a>
+</div>
 """)
 
 
